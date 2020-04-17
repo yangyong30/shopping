337a144c1c2c420ff16f51f27c4b7e4ea7f59374
yangyong30 <16619945883@163.com>
2020-04-17 13:13:40 +0800

    operate1

---
+++ src/components/Login.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="login_box"></div>
+</template>
+<script>
+export default {}
+</script>
+<style lang="less" scoped>
+.login_box {
+  height: 500px;
+  background-color: red;
+}
+</style>
